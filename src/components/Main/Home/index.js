9e15b11ce5459d5f2b01beaadbb9d4ecd62ab27e
agulujan41 <agulujan41proj@gmail.com>
2024-05-24 13:01:07 -0300

    adding some changes

--- src/components/Main/Home/index.js
+++ src/components/Main/Home/index.js
@@ -4,7 +4,7 @@ import HomeImg from "../../../images/home.jpg";
 import { languages } from "../../../data/constants";
 import { useGlobalState } from "../../../App";
 import { useTheme } from "styled-components";
-
+import "./style.css";
 const Home = () => {
   const defaultLanguageItem = useGlobalState("defaultLanguageItem")[0];
   const theme = useTheme();
@@ -29,7 +29,6 @@ const Home = () => {
     @media screen and (max-width: 768px) {
       height: 100%;
     }
-   
   `;
   const SpanTitle = styled.span`
     width: 500px;
@@ -42,80 +41,107 @@ const Home = () => {
     font-weight: 550;
     z-index: 20;
     font-size: 2.5em;
-    
+
     -webkit-transition: all 2s ease;
     -moz-transition: all 2s ease;
     -ms-transition: all 2s ease;
     transition: all 2s ease;
     animation: linear 1s appear_left_to_right;
-    
+
     @media screen and (max-width: 1100px) {
       top: 4%;
-      font-size:2em;
+      font-size: 2em;
       width: 300px;
     }
     @media screen and (max-width: 768px) {
       top: 3.5%;
-      font-size:1.85em;
+      font-size: 1.85em;
       width: 270px;
     }
     @media screen and (max-height: 450px) {
       top: 1.5%;
-      font-size:1.1em;
+      font-size: 1.1em;
       width: 200px;
     }
-    
-    
-    color: ${({ theme }) => theme.backgroundColor} !important;
 
-  
+    color: ${({ theme }) => theme.backgroundColor} !important;
   `;
   const SearchContainer = styled.div`
     position: absolute;
     bottom: 10%;
-    width: 60%;
+    
     left: 50%;
     transform: translateX(-50%);
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap:15px;
+    gap: 15px;
+
     
-    @media screen and (max-width: 1100px) {
-     
-      width:80%;
-    }
-    @media screen and (max-width: 768px) {
-      
-      width:85%;
-    }
-    @media screen and (max-height: 450px) {
-      width:75%;
-    }
   `;
-  
+
   const ButtonSearch = styled.a`
     text-decoration: none;
     text-size: 1.3em;
     background-color: ${({ theme }) => theme.buttonPrimaryColor};
     color: ${({ theme }) => theme.backgroundColor};
     padding: 10px 23px 10px 23px;
-    border-radius:23px;
+    border-radius: 23px;
+    cursor:pointer;
   `;
   const ContainerSearchComponent = styled.div`
-    height:70px;
-    background-color:${({theme})=>theme.backgroundColor};
+   
+    background-color: ${({ theme }) => theme.backgroundColor};
+    border-radius: 30px;
     width:100%;
-    border-radius:30px;
-
+    padding:10px 40px 10px 40px;
     @media screen and (max-width: 768px) {
-      
-      display:none;
+      display: none;
     }
-    @media screen and (max-height: 450px) {
-      height:50;
-    }
+   
+  `;
+  const TopSearchViewContainer = styled.div`
+   
+    display:flex;
+    flex-dirextion:row;
+    justify-content:space-between;
+    align-items:center;
+  `;
+  const QuestionsContainer = styled.div`
+    display:flex;
+    flex-dirextion:row;
+    justify-content:space-around;
+   
+    gap:10px;
+  `;
+  const Question = styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:space-around;
+    height:min-content;
+    padding-left:8px;
+    padding-right:8px;
+    margin:auto;
+  `;
+  const QuestionTitle = styled.span`
+    font-size:0.8em;
+    font-weight:bold;
+    color:${({theme})=>theme.primaryColor};
+    text-wrap:nowrap;
+  `;
+  const QuestionContent = styled.span`
+  font-size:0.7em;
+  color:${({theme})=>theme.secondaryColor};
+  color:gray;
+  text-wrap:nowrap;
+  `;
+  const LineaV = styled.div`
+    width:1px;
+    margin:auto;
+    height:45px;
+    background-color: ${({theme})=>theme.primaryColor};
+    
   `;
   return (
     <HomeContainer>
@@ -125,7 +151,36 @@ const Home = () => {
       </SpanTitle>
       <SearchContainer>
         <ContainerSearchComponent>
-
+          <input
+            type="search"
+            name=""
+            id=""
+            className="SearchMain"
+            placeholder="Search the next place you gonna travel"
+          ></input>
+          <TopSearchViewContainer>
+              <QuestionsContainer>
+                <Question>
+                    <QuestionTitle>Place</QuestionTitle>
+                    <QuestionContent>Search your destination</QuestionContent>
+                </Question>
+                < LineaV></LineaV>
+                <Question>
+                    <QuestionTitle>Place</QuestionTitle>
+                    <QuestionContent>Search your destination</QuestionContent>
+                </Question>
+                < LineaV></LineaV>
+                <Question>
+                    <QuestionTitle>Place</QuestionTitle>
+                    <QuestionContent>Search your destination</QuestionContent>
+                </Question>
+                < LineaV></LineaV>
+                <Question>
+                    <QuestionTitle>Place</QuestionTitle>
+                    <QuestionContent>Search your destination</QuestionContent>
+                </Question>
+              </QuestionsContainer>
+          </TopSearchViewContainer>
         </ContainerSearchComponent>
         <ButtonSearch>
           {languages[defaultLanguageItem]?.contents.search_button}
