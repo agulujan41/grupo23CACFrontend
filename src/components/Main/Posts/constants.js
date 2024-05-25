a726e81f85c33a024086b0e116afe8220fbe7c23
Brian Exequiel Putallaz <brianexequielputallaz@gmail.com>
2024-05-24 22:52:55 -0300

    Add language in section posts

---
+++ src/components/Main/Posts/constants.js
@@ -0,0 +1,52 @@
+
+export const languages= [
+    {
+        id:"en",
+        data: "English",
+        url_pic : "english.png",
+        contents:{
+            for_you: "For you:",
+            your_favorites: "Your favorites:",
+            most_views: "Most views:",
+            post_para: "Miami beaches are renowned for their sun-kissed sands, azure waters.",
+            post_button: "Add to shipping"
+            
+        }
+    },
+    {
+        id:"es",
+        data: "Español",
+        url_pic : "spanish.png",
+        contents:{
+            for_you: "Para tí:",
+            your_favorites: "Tus favoritos:",
+            most_views: "Más vistos",
+            post_para: "Las playas de Miami son famosas por sus arenas bañadas por el sol y aguas azules.",
+            post_button: "Agregar al carrito"
+        }
+    },
+    {
+        id:"fr",
+        data: "Français",
+        url_pic : "french.png",
+        contents:{
+            for_you: "Pour vous:",
+            your_favorites: "Vos favoris:",
+            most_views: "Les vues les plus:",
+            post_para: "Les plages de Miami sont renommées pour leurs sables baignés de soleil et leurs eaux azurées.",
+            post_button: "Ajouter au panier"
+        }
+    },
+    {
+        id:"ru",
+        data: "русский",
+        url_pic : "russian.png",
+        contents:{
+            for_you: "Для вас:",
+            your_favorites: "ваши любимые",
+            most_views: "Самые просмотры:",
+            post_para: "Пляжи Майами известны своими поцелованными солнцем песками и лазурными водами.",
+            post_button: "Добавить в корзину"
+        }
+    }
+];
