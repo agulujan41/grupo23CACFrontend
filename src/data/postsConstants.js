d9fad16f581505b9f34125617347df4db35abf16
agulujan41 <agulujan41proj@gmail.com>
2024-05-29 18:03:03 -0300

    adding constants posts

---
+++ src/data/postsConstants.js
@@ -0,0 +1,138 @@
+
+export const postsList= [
+    {
+        id:"en",
+        data: "English",
+        url_pic : "english.png",
+        contents:[
+            {
+                category:"For you:",
+                posts:[
+                    {
+                        post_title:"Miami Beach",
+                        post_para: "Miami beaches are renowned for their sun-kissed sands, azure waters.",
+                        img1:"miami_beach1.jpg",
+                        img2:"miami_beach2.jpg",
+                        img3:"miami_beach3.jpg",
+                        img4:"miami_beach4.jpg",
+                    }
+                   
+                ]
+
+            },
+            {
+                category:"Your favorites:",
+                posts:[
+                    {
+                        post_price:130,
+                        post_title:"Miami Beach",
+                        post_para: "Miami beaches are renowned for their sun-kissed sands, azure waters.",
+                        img1:"miami_beach1.jpg",
+                        img2:"miami_beach2.jpg",
+                        img3:"miami_beach3.jpg",
+                        img4:"miami_beach4.jpg",
+                    }
+                   
+                ]
+
+            },
+            {
+                category:"Most Views:",
+                posts:[
+                    {
+                        post_price:130,
+                        post_title:"Miami Beach",
+                        post_para: "Miami beaches are renowned for their sun-kissed sands, azure waters.",
+                        img1:"miami_beach1.jpg",
+                        img2:"miami_beach2.jpg",
+                        img3:"miami_beach3.jpg",
+                        img4:"miami_beach4.jpg",
+                    },
+                    {
+                        post_price:213,
+                        post_title:"Paris",
+                        post_para: "Welcome to the exquisite 'Elysée Palace Hotel,' nestled in the heart of Paris, the City of Light. Our luxurious hotel offers an enchanting blend of historic charm and modern elegance, ensuring a truly unforgettable stay. Located just steps away from iconic landmarks such as the Eiffel Tower, the Champs-Élysées, and the Louvre Museum, Elysée Palace Hotel provides an ideal base for exploring the rich culture, art, and history of this magnificent city.",
+                        img1:"paris1.jpg",
+                        img2:"paris2.jpg",
+                        img3:"paris3.jpg",
+                        img4:"paris4.jpg",
+                    },
+                    {
+                        post_price:199,
+                        post_title:"New York",
+                        post_para: "Welcome to the luxurious 'Manhattan Grande Hotel,' situated in the vibrant heart of New York City. Our distinguished hotel offers a perfect blend of contemporary sophistication and timeless elegance, ensuring an unforgettable stay in the city that never sleeps. Conveniently located near iconic attractions such as Times Square, Central Park, and Broadway, Manhattan Grande Hotel serves as the perfect base for exploring the endless wonders of this dynamic metropolis.",
+                        img1:"new_york1.jpg",
+                        img2:"new_york2.jpg",
+                        img3:"new_york3.jpg",
+                        img4:"new_york4.jpg",
+                    },
+                    {
+                        post_price:176,
+                        post_title:"Madrid",
+                        post_para: "Welcome to the elegant 'Madrid Royal Hotel', located in the heart of Spain's vibrant capital. Our prestigious hotel offers a seamless blend of classic Spanish charm and contemporary luxury, ensuring an unforgettable experience in this historic city. Situated near iconic landmarks such as the Royal Palace, Puerta del Sol, and the Prado Museum, Madrid Royal Hotel is the perfect gateway to explore the rich culture, art, and history of Madrid.",
+                        img1:"madrid1.jpg",
+                        img2:"madrid2.jpg",
+                        img3:"madrid3.jpg",
+                        img4:"madrid4.jpg",
+                    },
+                    {
+                        post_price:250,
+                        post_title:"London",
+                        post_para: "Welcome to the prestigious 'London Regent Hotel,' perfectly situated in the heart of England's vibrant capital. Our luxurious hotel seamlessly combines timeless British elegance with contemporary comforts, ensuring an unforgettable stay in one of the world's most iconic cities. Ideally located near major attractions such as Buckingham Palace, the British Museum, and the West End theatres, London Regent Hotel provides an excellent base for exploring the rich heritage and modern allure of London.",
+                        img1:"london1.jpg",
+                        img2:"london2.jpg",
+                        img3:"london3.jpg",
+                        img4:"london4.jpg",
+                    },
+                    {
+                        post_price:399,
+                        post_title:"Dubai",
+                        post_para: "Welcome to the opulent 'Dubai Majesty Hotel,' located in the heart of the United Arab Emirates' dazzling metropolis. Our luxurious hotel offers a perfect blend of modern extravagance and Arabian elegance, ensuring an unforgettable stay in one of the world's most dynamic cities. Ideally situated near iconic landmarks such as the Burj Khalifa, Dubai Mall, and the Palm Jumeirah, Dubai Majesty Hotel serves as the perfect gateway to explore the unparalleled wonders of Dubai.",
+                        img1:"dubai1.jpg",
+                        img2:"dubai2.jpg",
+                        img3:"dubai3.jpg",
+                        img4:"dubai4.jpg",
+                    },
+                ]
+
+            },
+        ]   
+        
+    },
+    {
+        id:"es",
+        data: "Español",
+        url_pic : "spanish.png",
+        contents:{
+            for_you: "Para tí:",
+            your_favorites: "Tus favoritos:",
+            most_views: "Más vistos",
+            post_para: "Las playas de Miami son famosas por sus arenas bañadas por el sol y aguas azules.",
+            post_button: "Agregar al carrito"
+        }
+    },
+    {
+        id:"fr",
+        data: "Français",
+        url_pic : "french.png",
+        contents:{
+            for_you: "Pour vous:",
+            your_favorites: "Vos favoris:",
+            most_views: "Les vues les plus:",
+            post_para: "Les plages de Miami sont renommées pour leurs sables baignés de soleil et leurs eaux azurées.",
+            post_button: "Ajouter au panier"
+        }
+    },
+    {
+        id:"ru",
+        data: "русский",
+        url_pic : "russian.png",
+        contents:{
+            for_you: "Для вас:",
+            your_favorites: "ваши любимые",
+            most_views: "Самые просмотры:",
+            post_para: "Пляжи Майами известны своими поцелованными солнцем песками и лазурными водами.",
+            post_button: "Добавить в корзину"
+        }
+    }
+];
